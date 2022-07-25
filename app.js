b1ad4a117750bd98b1d3633538deaa8cc37f4133
ansangyu <moto1515@gmail.com>
2022-07-25 14:44:10 +0900

    express gitignore 세팅

---
+++ app.js
@@ -0,0 +1,31 @@
+// app.js
+import express from "express";
+import mysql from "mysql2/promise";
+const app = express();
+const port = 3000;
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "sbsst",
+  password: "sbs123414",
+  database: "a9",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+app.get("/todos", async (req, res) => {
+  const [rows] = await pool.query("SELECT * FROM todo ORDER BY id DESC");
+  console.log("rows", rows);
+
+  res.json(rows);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!!");
+});
+("Str");
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
